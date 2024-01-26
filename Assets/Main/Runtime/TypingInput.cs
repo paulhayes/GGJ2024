75e8a4f819201e1035394dfa2b08d34a20a8032b
Hjalte Tagmose <HjalteTagmose@users.noreply.github.com>
2024-01-26 23:26:56 +0100

    make typephrases method

--- Assets/Main/Runtime/TypingInput.cs
+++ Assets/Main/Runtime/TypingInput.cs
@@ -1,17 +1,59 @@
+using System;
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class TypingInput : MonoBehaviour
 {
+	public event Action<string> OnSuccessfullyTypedWord;
+	public event Action OnTimeout;
 
-    public void TypeWords(string[] phrases)
+	private void Start()
+	{
+		TypePhrases(new[] { "Test" });
+	}
+
+	public void TypePhrases(string[] phrases)
     {
-        StartCoroutine(TypeWordsCoroutine(phrases));
+        StartCoroutine(TypePhrasesCoroutine(phrases));
     }
 
-    IEnumerator TypeWordsCoroutine(string[] phrases)
-    {
-        yield break;
-    }
+	private IEnumerator TypePhrasesCoroutine(string[] phrases)
+	{
+		var timer = 0;
+		int count = phrases.Length;
+		int[] idx = new int[count];
+
+		while (timer < 52)
+		{
+			string input = Input.inputString.ToLower();
+			
+			for (int i = 0; i < count; i++)
+			{
+				string phrase = phrases[i].ToLower();
+				int index = idx[i];
+
+				foreach (var letter in input)
+				{
+					print(phrase[index] + "");
+
+					if (phrase[index] == letter) //can go out of bounds
+					{
+						idx[i] = ++index;
+						print(phrase.Substring(0, index));
+					}
+
+					if (idx[i] >= phrase.Length)
+					{
+						OnSuccessfullyTypedWord?.Invoke(phrase);
+						print($"Successfully wrote: {phrase}");
+						yield break;
+					}
+				}
+			}
+
+			yield return null;
+		}
+
+		OnTimeout?.Invoke();
+	}
 }
