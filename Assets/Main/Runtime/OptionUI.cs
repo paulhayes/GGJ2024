084a017a884e2a2c81b0a8de8d71387fe8f6d8a4
Hjalte Tagmose <HjalteTagmose@users.noreply.github.com>
2024-01-27 22:33:45 +0100

    option juice

--- Assets/Main/Runtime/OptionUI.cs
+++ Assets/Main/Runtime/OptionUI.cs
@@ -1,6 +1,7 @@
 using System;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class OptionUI : MonoBehaviour
 {
@@ -8,6 +9,12 @@ public class OptionUI : MonoBehaviour
 	public TextMeshProUGUI backgroundText;
 	
 	private Option option;
+	private Image panel;
+
+	private void Awake()
+	{
+		panel = GetComponentInChildren<Image>();
+	}
 
 	public void SetOption(Option option)
 	{
@@ -24,5 +31,12 @@ public class OptionUI : MonoBehaviour
 
 		typingText.fontSize = backgroundText.fontSize;
 		typingText.text = option.GetTyped();
-    }
+
+		float pct = option.CompletionPct;
+		float opacity = Mathf.Lerp(.5f, 1f, pct);
+		float scale = Mathf.Lerp(1, 1.3f, pct);
+
+		transform.localScale = Vector3.one * scale;
+		panel.color = new Color(1,1,1,opacity);
+	}
 }
