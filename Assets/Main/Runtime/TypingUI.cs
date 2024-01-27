20cc43b284bdcc361fb57f9903f46a43a478fee3
Hjalte Tagmose <HjalteTagmose@users.noreply.github.com>
2024-01-27 14:04:21 +0100

    fix fmod

---
+++ Assets/Main/Runtime/TypingUI.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TypingUI : MonoBehaviour
+{
+	public OptionUI optionPrefab;
+	
+	public List<OptionUI> options;
+	public RectTransform[] points;
+
+	private void Start()
+	{
+
+	}
+
+	public void ShowOptions(Option[] options)
+	{
+
+	}
+}
